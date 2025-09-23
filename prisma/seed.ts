dc5e9aeff225b20a6448f94d49a6184ad6b170ef
Elsheshtawey1 <mohamedelsheshtawey1@gmail.com>
2025-09-23 15:01:23 +0300

    feat(todo): add Todo model, database query, and seed with faker

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,18 +3,31 @@ import { faker } from '@faker-js/faker';
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.user.createMany({
+// add todos to db from faker
+  await prisma.todo.createMany({
     data: Array.from({ length: 30 }, () => ({
-      name: faker.internet.username(),
-      email: faker.internet.email(),
-      address: {
-        street: faker.location.street(),
-        city: faker.location.city(),
-        state: faker.location.state(),
-        zip: faker.location.zipCode(),
-      },
+      title: faker.lorem.text(),
+      body: faker.lorem.paragraph(),
     })),
   });
+
+
+
+
+
+  // add users to db from faker
+  // await prisma.user.createMany({
+  //   data: Array.from({ length: 30 }, () => ({
+  //     name: faker.internet.username(),
+  //     email: faker.internet.email(),
+  //     address: {
+  //       street: faker.location.street(),
+  //       city: faker.location.city(),
+  //       state: faker.location.state(),
+  //       zip: faker.location.zipCode(),
+  //     },
+  //   })),
+  // });
 }
 
 main()
