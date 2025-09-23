8b587e1d8339eeb622c677ab03cd0c16ca663c26
Elsheshtawey1 <mohamedelsheshtawey1@gmail.com>
2025-09-23 14:15:48 +0300

    feat(seed): add ts-node and prisma seed with faker for generating fake records

---
+++ prisma/seed.ts
@@ -0,0 +1,27 @@
+import { PrismaClient } from "@prisma/client";
+import { faker } from '@faker-js/faker';
+const prisma = new PrismaClient();
+
+async function main() {
+  await prisma.user.createMany({
+    data: Array.from({ length: 30 }, () => ({
+      name: faker.internet.username(),
+      email: faker.internet.email(),
+      address: {
+        street: faker.location.street(),
+        city: faker.location.city(),
+        state: faker.location.state(),
+        zip: faker.location.zipCode(),
+      },
+    })),
+  });
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
