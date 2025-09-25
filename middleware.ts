90f83a236810efedc937b92b792f94d39b590b4e
Elsheshtawey1 <mohamedelsheshtawey1@gmail.com>
2025-09-26 01:19:23 +0300

    chore(auth): integrate Clerk and update schema to make userId optional

---
+++ middleware.ts
@@ -0,0 +1,18 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) {
+    await auth.protect();
+  }
+});
+
+export const config = {
+  matcher: [
+    // استثناء ملفات Next.js الداخلية والـ static assets
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // تشغيل الميدل وير على API routes
+    "/(api|trpc)(.*)",
+  ],
+};
